e2096fd5457749035e6da63f40e4913cd8d48d23
DiogoCezario-Dev <152320636+DiogoCezario-Dev@users.noreply.github.com>
2024-01-03 21:11:03 -0300

    up

---
+++ testes pessoais/teste.botoes/index.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".b1");
+var button2 = document.querySelector(".b2");
+
+button.onclick = function() {
+ window.open('www.google.com', '_blank');
+ button2.style.display = "block";
+}
